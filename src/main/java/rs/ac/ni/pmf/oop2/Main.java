4b703f87cd199f123deab93024a60e5b941167b3
Vladimir <vladimirjankovic016@gmail.com>
2023-11-14 11:25:42 +0100

    asddsad

---
+++ src/main/java/rs/ac/ni/pmf/oop2/Main.java
@@ -0,0 +1,28 @@
+package rs.ac.ni.pmf.oop2;
+
+public class Main {
+    public static void main(String[] args) {
+        final ExpresssionPlus exp = new ExpresssionPlus();
+        final ExpressionCalculator calculator = new ExpressionCalculator(4,6,exp);
+        System.out.println(calculator.calculate());
+
+        class ExpressionSquareSum implements Expression {
+            @Override
+            public int calculate(int a, int b) {
+                return (a + b) * (a + b);
+            }
+        }
+        ExpressionSquareSum ess = new ExpressionSquareSum();
+        final ExpressionCalculator otherCalc =
+                new ExpressionCalculator(10,10,ess);
+
+
+        final ExpressionCalculator otherCalc2 =
+                new ExpressionCalculator(10, 10, new Expression() {
+                    @Override
+                    public int calculate(int a, int b) {
+                        return a*a - b*b;
+                    }
+                });
+    }
+}
